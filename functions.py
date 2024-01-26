8b24762c55f28659bf3dfdda2ba4ca35ac4c94d5
Vitor Miranda <vmaquino77@gmail.com>
2024-01-26 09:31:21 -0300

    Adicionar função de baixar orçamento

--- functions.py
+++ functions.py
@@ -9,57 +9,212 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import UnexpectedAlertPresentException
 import streamlit as st
 
-def configuracao(caminho, num_processo):
+def configurar_navegador_para_download(caminho, num_processo):
+    """
+    Configura e inicializa um navegador Chrome para automatizar o download de arquivos.
+    
+    Parâmetros:
+    caminho (str): Caminho base onde os arquivos serão baixados.
+    num_processo (str): Identificador do processo, usado para criar uma subpasta no caminho de download.
+
+    Retorna:
+    webdriver.Chrome: Uma instância do navegador Chrome configurada com as opções de download especificadas.
+    
+    Esta função instala o ChromeDriver, configura as opções de download para o navegador
+    Chrome e retorna a instância do navegador. O diretório de download é definido como uma
+    subpasta dentro do caminho fornecido, baseado no número do processo.
+    """
+
+    # Instala o ChromeDriver e inicializa o serviço
     servico = Service(ChromeDriverManager().install())
 
-    # Set up Chrome options
+    # Configura as opções do navegador Chrome
     chrome_options = webdriver.ChromeOptions()
 
-    # Specify the download directory
+    # Especifica o diretório de download com base no caminho e número do processo
     download_directory = caminho + f"\\{str(num_processo)}"
-    # Adicione a opção de preferência
+
+    # Define as preferências de download para o navegador
     prefs = {"download.default_directory": download_directory}
     chrome_options.add_experimental_option("prefs", prefs)
 
-    navegador =  webdriver.Chrome(service=servico, options=chrome_options)
+    # Inicializa o navegador Chrome com as opções configuradas
+    navegador = webdriver.Chrome(service=servico, options=chrome_options)
 
     return navegador
 
+def realizar_login_liberty(navegador, login, senha):
+    """
+    Realiza o login no site da Liberty Seguros.
 
-def login(navegador, login_allianz, senha_allianz):
-    # Indo para o site da liberty
-    navegador.get(url="https://ressarcimentofianca.libertyseguros.com.br/login")
+    Parâmetros:
+    navegador (webdriver.Chrome): Uma instância do navegador Chrome.
+    login (str): Nome de usuário para login.
+    senha (str): Senha para login.
 
-    time.sleep(3)
-    # Inserindo login
-    navegador.find_element('xpath', '/html/body/app-root/app-login-prestador/div[2]/div/div/div[2]/div[2]/div/div[1]/input').send_keys(login_allianz)
-    # Inserindo senha
-    navegador.find_element('xpath', '/html/body/app-root/app-login-prestador/div[2]/div/div/div[2]/div[2]/div/div[2]/input').send_keys(senha_allianz)
+    Retorna:
+    webdriver.Chrome: A instância do navegador com a sessão de login iniciada.
 
-    time.sleep(3)
-    # Logando no conta
-    navegador.find_element('xpath', '/html/body/app-root/app-login-prestador/div[2]/div/div/div[2]/div[2]/div/div[3]/input').click()
+    Esta função acessa o site de ressarcimento da Liberty Seguros, aguarda a presença dos elementos
+    de login e senha, insere as credenciais e realiza o login.
+    """
+
+    # Acessa o site da Liberty Seguros
+    navegador.get("https://ressarcimentofianca.libertyseguros.com.br/login")
+
+    # Abrir tela cheia
+    navegador.maximize_window()
+
+    # Espera pela presença do campo de login e insere o login
+    WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-login-prestador/div[2]/div/div/div[2]/div[2]/div/div[1]/input'))).send_keys(login)
+
+    # Espera pela presença do campo de senha e insere a senha
+    WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-login-prestador/div[2]/div/div/div[2]/div[2]/div/div[2]/input'))).send_keys(senha)
+
+    # Espera pela presença do botão de login e clica nele
+    WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-login-prestador/div[2]/div/div/div[2]/div[2]/div/div[3]/input'))).click()
 
     return navegador
 
-def localizando_processo(navegador, num_processo):
-    # Adicionando o número do processo
+def localizar_processo(navegador, num_processo):
+    """
+    Localiza um processo no site especificado, utilizando o número do processo.
+
+    Parâmetros:
+    navegador (webdriver.Chrome): Uma instância do navegador Chrome.
+    num_processo (str): Número do processo a ser pesquisado.
+
+    Retorna:
+    webdriver.Chrome: A instância do navegador após a pesquisa do processo.
+
+    Esta função aguarda a presença do campo de pesquisa, insere o número do processo,
+    aguarda a presença do botão de pesquisa e clica nele para realizar a busca.
+    """
+
+    # Aguarda pela presença do campo de pesquisa e insere o número do processo
     search_box = WebDriverWait(navegador, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="pesquisa"]'))
     )
     search_box.send_keys(num_processo)
 
-    # Pesquisando o processo
+    # Aguarda pela presença do botão de pesquisa e clica nele
     search_button = WebDriverWait(navegador, 10).until(
         EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-pesquisa/div[2]/div[3]/div[2]/button[1]'))
     )
     search_button.click()
+
+    return navegador
+
+def baixar_orcamento(navegador):
+    """
+    Navega em um site específico e automatiza o processo de download de orçamentos e fotos.
+
+    Parâmetros:
+    navegador (webdriver.Chrome): Uma instância do navegador Chrome controlada pelo Selenium.
+
+    O processo inclui:
+    - Clicar no botão para ir à página de orçamento.
+    - Aguardar e clicar no botão de relatório completo, o que abre uma nova aba.
+    - Mudar para a nova aba e clicar no botão de download do PDF do orçamento.
+    - Esperar a finalização do download e fechar a aba.
+    - Retornar para a aba anterior e clicar no botão de visualização de fotos do orçamento.
+    - Selecionar todas as fotos disponíveis e iniciar o download.
+    - Fechar a aba de fotos e retornar para a aba original.
+
+    Retorna:
+    webdriver.Chrome: A instância do navegador após a execução das operações de download.
+
+    Notas:
+    - A função utiliza `time.sleep` para intervalos fixos entre as operações, o que pode ser
+      substituído por esperas mais dinâmicas do Selenium em futuras otimizações.
+    - A função depende da estrutura específica do site e dos seletores XPATH utilizados, o que
+      pode requerer ajustes caso haja mudanças no layout ou no comportamento do site.
+    """
+    
+    time.sleep(7)
+    # Ir para página de orçamento
+    orcamento_botao = WebDriverWait(navegador, 15).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-ressarcimento/app-footer/footer/button[4]'))
+    )
+    orcamento_botao.click()
+
+    time.sleep(5)
+    # Alterar o navegador para a segunda aba
+    WebDriverWait(navegador, 10).until(
+        EC.number_of_windows_to_be(2))
+    new_window_handle = navegador.window_handles[1]
+    navegador.switch_to.window(new_window_handle)
+
+    #time.sleep(7)
+    # Clicar no botão de relatório completo
+    relatorio_botao = WebDriverWait(navegador, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="budget_report"]/div/div[2]/a/center')))
+    relatorio_botao.click()
+
+
+    # Alterar o navegador para terceira aba
+    WebDriverWait(navegador, 10).until(
+        EC.number_of_windows_to_be(3))
+    new_window_handle = navegador.window_handles[2]
+    navegador.switch_to.window(new_window_handle)
+
+    # Clicar no botão de PDF
+    relatorio_pdf_botao = WebDriverWait(navegador, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="btn_pdf_report"]')))
+    relatorio_pdf_botao.click()
+
+    time.sleep(8)
+
+    # Fechar a terceira aba
+    navegador.close()
+
+    # Retornar para a segunda a aba
+    WebDriverWait(navegador, 10).until(
+        EC.number_of_windows_to_be(2))
+    new_window_handle = navegador.window_handles[1]
+    navegador.switch_to.window(new_window_handle)
+
+
+    # Clicar no botão de fotos
+    orcamento_fotos_botao = WebDriverWait(navegador, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="photos_menu"]')))
+    orcamento_fotos_botao.click()
+
+    time.sleep(5)
+
+
+    # Clicar no botão para selecionar todas as fotos
+    todas_fotos_botao = WebDriverWait(navegador, 20).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="budgeting-photos-content"]/div[1]/div[2]/div/label')))
+    todas_fotos_botao.click()
+
+    # Fazer o download das fotos
+    baixar_fotos_botao = WebDriverWait(navegador, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="budgeting-photos-print"]/div/a[2]')))
+    baixar_fotos_botao.click()
+
+    time.sleep(8)
+
+    # Fechar a segunda aba
+    navegador.close()
+
+    # Retornar para primeira aba
+    WebDriverWait(navegador, 10).until(
+        EC.number_of_windows_to_be(1))
+    new_window_handle = navegador.window_handles[0]
+    navegador.switch_to.window(new_window_handle)
+
+
+    return navegador
+
+def downloads(navegador):
 
     # Indo para o site com os arquivos
     files_button = WebDriverWait(navegador, 10).until(
         EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-ressarcimento/app-footer/footer/button[6]'))
     )
     files_button.click()
+    
 
     # Mudando para aba com os documentos
     WebDriverWait(navegador, 10).until(
@@ -69,14 +224,6 @@ def localizando_processo(navegador, num_processo):
     navegador.switch_to.window(new_window_handle)
 
     time.sleep(10)
-
-    return navegador
-
-
-
-
-
-def downloads(navegador):
 
     documentos_baixados = 0
     
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from functions import configuracao, login, downloads, localizando_processo
+from functions import configurar_navegador_para_download, realizar_login_liberty, localizar_processo, baixar_orcamento, downloads
 import pandas as pd
 
 st.set_page_config(
@@ -17,8 +17,13 @@ if login_credencial is not None and senha_credencial is not None:
     st.subheader("Caminho para salvar")
     #caminho = st.text_input("Digite o caminho para serem salvados os arquivos: [Usar duas barras ao invés de uma só]", value=None)
     caminho = r'C:\Users\Vitor\Documents\Repositórios\automação\ressarcimento\dados'
+
     if caminho is not None:
         st.write(caminho)
+
+        st.subheader("Orçamento")
+        opcao_orcamento = st.selectbox("Você deseja baixar as informações do orçamento?", ("Não", "Sim"))
+
         st.subheader("Processo")
         excel_file = st.file_uploader('Insira um arquivo com o número dos processos', type='xlsx')
 
@@ -28,22 +33,41 @@ if login_credencial is not None and senha_credencial is not None:
             df_processo_show['Processo'] = df_processo_show['Processo'].astype('str')
 
             st.subheader("Procedimento")
+            lista_docs_baixados = []
+            lista_docs_problema = []
             with st.status("Fazendo download dos arquivos ..."):
-                lista_docs_baixados = []
-                lista_docs_problema = []
+                
                 for processo in df_processo.Processo:
-                #for processo in [16247971, 16339902]:
+
                     st.write(f'**Iniciando procedimento para o processo: {processo}**')
-                    navegador = configuracao(caminho, processo)
+                    navegador = configurar_navegador_para_download(caminho, processo)
+
                     st.write("Configuração concluída")
-                    navegador = login(navegador, login_credencial, senha_credencial)
+                    navegador = realizar_login_liberty(navegador, login_credencial, senha_credencial)
+
                     st.write("Login concluído.")
-                    navegador = localizando_processo(navegador, processo)
+                    navegador = localizar_processo(navegador, processo)
+
+                    if opcao_orcamento == "Sim":
+                        try:
+                            navegador = baixar_orcamento(navegador)
+                            st.write("Download do orçamento concluído.")
+                        except:
+                            navegador.quit()
+                            lista_docs_problema.append(processo)
+                            st.write("Problema no download do orçamento.")
+                            navegador = configurar_navegador_para_download(caminho, processo)
+                            navegador = realizar_login_liberty(navegador, login_credencial, senha_credencial)
+                            navegador = localizar_processo(navegador, processo)
+
+
                     documentos_baixados, flag_problema = downloads(navegador)
                     if flag_problema  == True:
                         lista_docs_problema.append(processo)
                     lista_docs_baixados.append(documentos_baixados)
-                    st.write("Download concluído.")
+                    st.write("Download dos documentos concluído.")
+
+                    
             st.subheader("Geral")
             df_processo_show['Documentos Baixados'] = lista_docs_baixados
             st.table(df_processo_show)
