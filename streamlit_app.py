c3b5deb5c4d43285efe1057124e52e64967c1383
Vitor Miranda <vmaquino77@gmail.com>
2024-06-09 16:30:53 -0300

    removendo tentativa de download do chrome

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,5 @@ import os
 
 if __name__ == "__main__":
 
-
-    @st.experimental_singleton
-    def install_chrome_driver():
-        os.system('apt-get update')
-        os.system('apt-get install -y chromium-driver')
-        os.system('ln -s /usr/lib/chromium-browser/chromedriver /usr/bin/chromedriver')
-
-    install_chrome_driver()
-
     app = WebApp()
     app.run()
